8a482e32881e2f9fb4abd2498590bd57a4d277f7
karnam-naveen80 <47709705+karnam-naveen80@users.noreply.github.com>
2019-03-31 17:25:57 +0530

    Update Naveen1.java

--- src/gitexample55/Naveen1.java
+++ src/gitexample55/Naveen1.java
@@ -6,7 +6,7 @@ public class Naveen1 {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Naveen Karnam\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		ChromeDriver d=new ChromeDriver();
+		ChromeDriver d=new ChromeDrive();
 		d.get("https://google.com");
 
 	}
