652458b641d7690098257af8508fb12434d83215
Naveen Karnam <Naveen Karnam@DESKTOP-AQFB0ON>
2019-03-31 17:21:26 +0530

    jii

--- src/gitexample55/Naveen1.java
+++ src/gitexample55/Naveen1.java
@@ -1,9 +1,13 @@
 package gitexample55;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Naveen1 {
 
-	public static void main(String[] arg) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Naveen Karnam\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver d=new ChromeDriver();
+		d.get("https://google.com");
 
 	}
 
